d0053c36366e7c6c6c0e14e8ce40905f0e3e265f
Joshua Novick <Joshnov7@gmail.com>
2025-08-20 14:17:58 +1000

    Fix import error: Remove non-existent DashboardRenderer import

--- app/visualization/__init__.py
+++ app/visualization/__init__.py
@@ -3,6 +3,5 @@ Visualization module for property analysis
 """
 
 from .charts import ChartGenerator
-from .dashboard import DashboardRenderer
 
-__all__ = ['ChartGenerator', 'DashboardRenderer']
+__all__ = ['ChartGenerator']
